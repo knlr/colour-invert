18acd6a4f6bec1365c4b85c9e42a38450db4eb7a
Knut Lorenzen <lo.knut@gmail.com>
2021-01-30 17:01:57 +0000

    show camera feed

--- ImageDemo/ViewController.swift
+++ ImageDemo/ViewController.swift
@@ -6,14 +6,61 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 
+    lazy var session = AVCaptureSession()
+    weak var previewLayer: AVCaptureVideoPreviewLayer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        AVCaptureDevice.requestAccess(for: .video) {
+
+            if $0 {
+                DispatchQueue.main.async {
+                    self.startCapture()
+                }
+            }
+        }
     }
 
 
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previewLayer?.frame = view.layer.bounds
+    }
+
+
+    func startCapture() {
+
+        let output = AVCapturePhotoOutput()
+        session.sessionPreset = .high
+        guard let device = AVCaptureDevice.default(for: .video)
+        else { return }
+        //        guard let device = AVCaptureDevice.devices(for: .video).first(where: { $0.position == .front })
+//        else { return }
+
+        do {
+            let input = try AVCaptureDeviceInput(device: device)
+            if session.canAddInput(input) &&
+                session.canAddOutput(output) {
+
+                session.addInput(input)
+                session.addOutput(output)
+
+                let layer = AVCaptureVideoPreviewLayer(session: session)
+                previewLayer = layer
+                layer.videoGravity = .resizeAspectFill
+                view.layer.addSublayer(layer)
+
+                session.startRunning()
+            }
+        }
+        catch {
+            print(error)
+        }
+    }
 }
 
